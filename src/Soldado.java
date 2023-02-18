cde21200f3d5370e0feef83660c17ca4e69f83ff
ksio7 <sergiusparedes@gmail.com>
2023-02-18 20:10:14 +0100

    creados los metodos con la documentacion

--- src/Soldado.java
+++ src/Soldado.java
@@ -1,7 +1,92 @@
+
+/**
+ * Clase que indica si un soldado puede disparar y si tiene balas, puede disparar y por lo tanto matar a su adversario, si dispara, resta una bala.
+ *  
+ * 
+ * 
+ * @author ksio7
+ * @version 1.0
+ */
+
+
+
 public class Soldado {
 
-              private boolean estaMuerto;
+	
+// ATRIBUTOS DE LA CLASE SOLDADO	
 
-              private int numeroBalas;
+	
+/**
+ * Esta propiedad indica si consigue matar o no 
+ */
+private boolean estaMuerto;
+
+/**
+ * Esta propiedad indica el numero de balas que tiene y por lo tanto si puede disparar o no 
+ */
+ private int numeroBalas;
+ 
+ 
+ 
+ //GETTER AND SETTER.
+ 
+public boolean isEstaMuerto() {
+	return estaMuerto;
+}
+
+public void setEstaMuerto(boolean estaMuerto) {
+	this.estaMuerto = estaMuerto;
+}
+
+public int getNumeroBalas() {
+	return numeroBalas;
+}
+
+public void setNumeroBalas(int numeroBalas) {
+	this.numeroBalas = numeroBalas;
+}
+ 
+/**
+ * Este metodo indica si el soldado tiene mas de 0 balas y por lo tanto si puede disparar.
+ * 
+ * @return si puede disparar true, si no, false.
+ */
+
+
+public boolean puedeDisparar() {
+
+    if(this.numeroBalas > 0) {
+
+                  return true;
+
+    }                          
+
+    return false;
+
+}
+
+
+/**
+ * Este metodo dispara a un soldado, 
+ * 
+ * @param sol representa al soldado al que se le dispara
+ */
+
+public void disparar(Soldado sol) {
+
+    this.numeroBalas--;
+
+    sol.estaMuerto = true;
+
+}
+
+
+
+
+
+
+
+
+ 
               
 }
